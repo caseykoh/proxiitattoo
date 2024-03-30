5dd7ad5e0976c8e5d57aff4724a503d1daf2bf34
caseykoh <s.casey.koh@gmail.com>
2024-03-29 21:36:39 -0400

    Organized form inputs

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -13,6 +13,7 @@ import {
   IoCloudUpload,
   IoSquareOutline,
 } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 
 const DesignTypeEnum = z.enum(["Flash", "Custom", "Freehand"]);
 type DesignTypeEnum = z.infer<typeof DesignTypeEnum>;
@@ -29,14 +30,14 @@ type DesignTypeEnum = z.infer<typeof DesignTypeEnum>;
 // });
 
 const schema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
+  name: z.string(),
   email: z.string(),
   instagram: z.string(),
   designType: DesignTypeEnum,
   description: z.string(),
   date: z.coerce.date(),
   reference: z.any(),
+  specs: z.string(),
 });
 
 type FormFields = z.infer<typeof schema>;
@@ -103,8 +104,7 @@ const Booking = () => {
     // };
     //formdata is for uploading files of different types, like images/files
     const formData = new FormData();
-    formData.append("first_name", data.firstName);
-    formData.append("last_name", data.lastName);
+    formData.append("first_name", data.name);
     formData.append("email", data.email);
     formData.append("instagram", data.instagram);
 
@@ -146,188 +146,210 @@ const Booking = () => {
               to place a booking fee to secure your appointment.
             </p>
           </div>
-          <div className="form-control">
-            <label>I'm looking for a...</label>
-            <div className="design-type-container">
-              <input
-                {...register("designType")}
-                type="radio"
-                value={DesignTypeEnum.enum.Flash}
-                id="flash-select"
-              />
-              <input
-                {...register("designType")}
-                type="radio"
-                value={DesignTypeEnum.enum.Custom}
-                id="custom-select"
-              />
-              <input
-                {...register("designType")}
-                type="radio"
-                value={DesignTypeEnum.enum.Freehand}
-                id="freehand-select"
-              />
-              <label
-                htmlFor="flash-select"
-                onClick={() => {
-                  if (!flashSelected) {
-                    setFlashSelected(!flashSelected);
-                    setCustomSelected(false);
-                    setFreehandSelected(false);
-                  }
-                }}
-              >
-                <div className="card">
-                  <span className="check-btn">
-                    {flashSelected ? <IoCheckbox /> : <IoSquareOutline />}
-                  </span>
-                  <div className="card-info">
-                    <h2 className="card-title">Flash</h2>
-                    <p>Choose a ready-made design from my flash.</p>
+          <div className="form-group">
+            <div className="form-control">
+              <label>I'm looking for a... *</label>
+              <div className="design-type-container">
+                <input
+                  {...register("designType")}
+                  type="radio"
+                  value={DesignTypeEnum.enum.Flash}
+                  id="flash-select"
+                />
+                <input
+                  {...register("designType")}
+                  type="radio"
+                  value={DesignTypeEnum.enum.Custom}
+                  id="custom-select"
+                />
+                <input
+                  {...register("designType")}
+                  type="radio"
+                  value={DesignTypeEnum.enum.Freehand}
+                  id="freehand-select"
+                />
+                <label
+                  htmlFor="flash-select"
+                  onClick={() => {
+                    if (!flashSelected) {
+                      setFlashSelected(!flashSelected);
+                      setCustomSelected(false);
+                      setFreehandSelected(false);
+                    }
+                  }}
+                >
+                  <div className="card">
+                    <span className="check-btn">
+                      {flashSelected ? <IoCheckbox /> : <IoSquareOutline />}
+                    </span>
+                    <div className="card-info">
+                      <h2 className="card-title">Flash</h2>
+                      <p>Choose a ready-made design from my flash.</p>
+                      <div className="flash-button">
+                        <NavLink to="/gallery" className="view-flash-link">
+                          view flash
+                        </NavLink>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </label>
-              <label
-                htmlFor="custom-select"
-                onClick={() => {
-                  if (!customSelected) {
-                    setFlashSelected(false);
-                    setCustomSelected(!customSelected);
-                    setFreehandSelected(false);
-                  }
-                }}
-              >
-                <div className="card">
-                  <span className="check-btn">
-                    {customSelected ? <IoCheckbox /> : <IoSquareOutline />}
-                  </span>
-                  <div className="card-info">
-                    <h2 className="card-title">Custom</h2>
-                    <p>
-                      Specify a placement and size with references, and a design
-                      will be custom made for you.
-                    </p>
+                </label>
+                <label
+                  htmlFor="custom-select"
+                  onClick={() => {
+                    if (!customSelected) {
+                      setFlashSelected(false);
+                      setCustomSelected(!customSelected);
+                      setFreehandSelected(false);
+                    }
+                  }}
+                >
+                  <div className="card">
+                    <span className="check-btn">
+                      {customSelected ? <IoCheckbox /> : <IoSquareOutline />}
+                    </span>
+                    <div className="card-info">
+                      <h2 className="card-title">Custom</h2>
+                      <p>
+                        Specify a placement and size with references, and a
+                        design will be custom made for you.
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </label>
-              <label
-                htmlFor="freehand-select"
-                onClick={() => {
-                  if (!freehandSelected) {
-                    setFlashSelected(false);
-                    setCustomSelected(false);
-                    setFreehandSelected(!freehandSelected);
-                  }
-                }}
-              >
-                <div className="card">
-                  <span className="check-btn">
-                    {freehandSelected ? <IoCheckbox /> : <IoSquareOutline />}
-                  </span>
-                  <div className="card-info">
-                    <h2 className="card-title">Freehand</h2>
-                    <p>
-                      For the spontaneous and utmost fan of my work: you show up
-                      and a design is drawn directly on skin and is then
-                      tattooed.
-                    </p>
+                </label>
+                <label
+                  htmlFor="freehand-select"
+                  onClick={() => {
+                    if (!freehandSelected) {
+                      setFlashSelected(false);
+                      setCustomSelected(false);
+                      setFreehandSelected(!freehandSelected);
+                    }
+                  }}
+                >
+                  <div className="card">
+                    <span className="check-btn">
+                      {freehandSelected ? <IoCheckbox /> : <IoSquareOutline />}
+                    </span>
+                    <div className="card-info">
+                      <h2 className="card-title">Freehand</h2>
+                      <p>
+                        For the spontaneous and utmost fan of my work: you show
+                        up and a design is drawn directly on skin and is then
+                        tattooed.
+                      </p>
+                    </div>
                   </div>
-                </div>
+                </label>
+              </div>
+            </div>
+            <div className="form-control">
+              <label>Image references *</label>
+              <input
+                {...register("reference")}
+                id="reference"
+                name="reference"
+                accept="image/*"
+                type="file"
+                multiple
+                onChange={onImageChange}
+              />
+              {images.length === 0 ? (
+                <label htmlFor="reference" className="empty-references">
+                  <IoCloudUpload className="upload-icon" />
+                  <span>Choose file</span>
+                </label>
+              ) : (
+                <label htmlFor="reference" className="add-references">
+                  {images.map((imageURL) => (
+                    <img alt="preview image" src={imageURL} />
+                  ))}
+
+                  <div className="add-image-btn">
+                    <IoCloudUpload className="upload-icon" />
+                    <span>Add image</span>
+                  </div>
+                </label>
+              )}
+            </div>
+            <div className="form-control">
+              <label>Where do you want it and how big? *</label>
+              <input
+                {...register("specs")}
+                type="text"
+                placeholder="Size + Placement"
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">
+                What's your vision for the piece?
               </label>
+              {/* check that this textarea also goes in data on submit */}
+              <textarea
+                {...register("description")}
+                id="description"
+                name="description"
+                placeholder="Describe your vision"
+                rows={4}
+              />
             </div>
           </div>
-          <div className="form-control">
-            <label>Image references</label>
-            <input
-              {...register("reference")}
-              id="reference"
-              name="reference"
-              accept="image/*"
-              type="file"
-              multiple
-              onChange={onImageChange}
-            />
-            {images.length === 0 ? (
-              <label htmlFor="reference" className="empty-references">
-                <IoCloudUpload className="upload-icon" />
-                <span>Choose file</span>
+          <div className="form-group">
+            <h3>Preferred Date</h3>
+            <div className="form-control">
+              <label htmlFor="date">
+                Select a date that works for your appointment *
               </label>
-            ) : (
-              <label htmlFor="reference" className="add-references">
-                {images.map((imageURL) => (
-                  <img alt="preview image" src={imageURL} />
-                ))}
+              <Controller
+                control={control}
+                name="date"
+                render={({ field }) => (
+                  <DatePicker
+                    filterDate={available}
+                    onChange={(date) => {
+                      date && setDate(date);
+                      field.onChange(date);
+                    }}
+                    inline
+                  />
+                )}
+              />
+            </div>
+          </div>
 
-                <div className="add-image-btn">
-                  <IoCloudUpload className="upload-icon" />
-                  <span>Add image</span>
-                </div>
-              </label>
-            )}
-          </div>
-          <div className="form-control">
-            <label htmlFor="description">Placement and design ideas</label>
-            {/* check that this textarea also goes in data on submit */}
-            <textarea
-              {...register("description")}
-              id="description"
-              name="description"
-              placeholder="Description"
-              rows={4}
-            />
-          </div>
-          <h3>Preferred Date</h3>
-          <div className="form-control">
-            <label htmlFor="date">Select a preferred date</label>
-            <Controller
-              control={control}
-              name="date"
-              render={({ field }) => (
-                <DatePicker
-                  filterDate={available}
-                  onChange={(date) => {
-                    date && setDate(date);
-                    field.onChange(date);
-                  }}
-                  inline
+          <div className="form-group">
+            <h3>Contact Info</h3>
+            <div className="contact-input-group">
+              <div className="form-control">
+                <label>Full Name *</label>
+                <input
+                  {...register("name")}
+                  type="text"
+                  placeholder="First Name"
                 />
-              )}
-            />
-          </div>
-          <h3>Client Info</h3>
-          <div className="form-control">
-            <label>First Name</label>
-            <input
-              {...register("firstName")}
-              type="text"
-              placeholder="First Name"
-            />
-            {errors.firstName && (
-              <div className="text-error">{errors.firstName.message}</div>
-            )}
-          </div>
-          <div className="form-control">
-            <label>Last Name</label>
-            <input
-              {...register("lastName")}
-              type="text"
-              placeholder="Last Name"
-            />
-          </div>
-          <div className="form-control">
-            <label>Email</label>
-            <input {...register("email")} type="text" placeholder="Email" />
-            {errors.email && (
-              <div className="text-error">{errors.email.message}</div>
-            )}
-          </div>
-          <div className="form-control">
-            <label>Instagram Handle</label>
-            <input
-              {...register("instagram", { required: true })}
-              type="text"
-              placeholder="@"
-            />
+                {errors.name && (
+                  <div className="text-error">{errors.name.message}</div>
+                )}
+              </div>
+              <div className="form-control">
+                <label>Email</label>
+                <input
+                  {...register("email")}
+                  type="text"
+                  placeholder="example@gmail.com"
+                />
+                {errors.email && (
+                  <div className="text-error">{errors.email.message}</div>
+                )}
+              </div>
+              <div className="form-control">
+                <label>Instagram Handle *</label>
+                <input
+                  {...register("instagram", { required: true })}
+                  type="text"
+                  placeholder="@"
+                />
+              </div>
+            </div>
           </div>
 
           <button disabled={isSubmitting} type="submit">
