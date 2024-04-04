f686df118653eed2436b80038eaf04f3acd34774
caseykoh <s.casey.koh@gmail.com>
2024-04-03 21:02:12 -0400

    Grid styling for single column

--- src/pages/FlashPage.tsx
+++ src/pages/FlashPage.tsx
@@ -4,14 +4,14 @@ import flashImages from "../components/FlashImages.tsx";
 const FlashPage = () => {
   return (
     <section className="gallery-container">
-      <h1>Flash Designs</h1>
-      <p>Available for booking.</p>
+      <h1>Flash</h1>
+      {/* <p>Available for booking.</p> */}
       <section className="photo-grid">
         {flashImages?.map((flashImg) => (
           <div className="card flash-card">
             <img src={flashImg.image} />
             <div className="img-caption">
-              <button className="book-btn">{"[ request flash ]"}</button>
+              <button className="book-btn">{"[request flash]"}</button>
             </div>
           </div>
         ))}
