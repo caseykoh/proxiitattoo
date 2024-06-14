21e62610eef3faba0bc59666322a4514f75d341e
Casey Koh <s.casey.koh@gmail.com>
2024-06-14 14:10:46 -0400

    Updated available flash ideas

--- src/components/FlashImages.tsx
+++ src/components/FlashImages.tsx
@@ -1,14 +1,9 @@
 export default [
   { image: "/assets/IMG_3473.png", title: "e73" },
   { image: "/assets/ponyflash.jpg", title: "pony" },
-  { image: "/assets/IMG_3472.png", title: "e72" },
-  { image: "/assets/IMG_3377.png", title: "e77" },
   { image: "/assets/circle.png", title: "circle" },
   { image: "/assets/IMG_3378.png", title: "e78" },
   { image: "/assets/IMG_3380.png", title: "e80" },
-  { image: "/assets/ripples.jpg", title: "ripples" },
   { image: "/assets/IMG_3379.png", title: "e79" },
-  { image: "/assets/IMG_3371.png", title: "e71" },
-  { image: "/assets/IMG_3372.png", title: "e72" },
   { image: "/assets/IMG_3370.png", title: "e70" },
 ];
