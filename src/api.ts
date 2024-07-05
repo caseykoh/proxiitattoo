64d25df0847c3d6e1446291138074cd908beb592
Casey Koh <s.casey.koh@gmail.com>
2024-07-05 10:06:26 -0400

    Api and environment file

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export async function uploadFileToS3(imageLink: File, url: string) {
+  //PUT request with the body as the file.
+  const response = await axios.put(url, imageLink, {
+    headers: {
+      "Content-Type": "application/png",
+    },
+  });
+  console.log(response);
+}
+
+export async function submitForm(formData: FormData) {
+  const response = await axios
+    .put("/appointments", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((result) => {
+      console.log(result.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(response);
+}
