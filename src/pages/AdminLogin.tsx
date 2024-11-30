b3e7dad00272488bcb0730e59551017d55408fd0
Casey Koh <s.casey.koh@gmail.com>
2024-11-30 11:58:53 -0500

    Set local storage token on form submit

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
 import { Lock } from "lucide-react";
 
 export default function AdminLoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the page from reloading
+    const loginDetails = { username, password };
+    // Call the API to authenticate the user and get the JWT token
+    const response = await axios
+      .post(import.meta.env.VITE_APP_API_ENDPOINT + "/auth/login", loginDetails)
+      .then((result) => {
+        console.log(result);
+        localStorage.setItem("jwt", result.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full px-6 py-8 bg-white shadow-lg rounded-lg">
@@ -13,20 +33,31 @@ export default function AdminLoginPage() {
             Please sign in to access the admin dashboard
           </p>
         </div>
-        <form className="space-y-4" action="/api/admin/login">
+        <form
+          className="space-y-4"
+          onSubmit={handleSubmit}
+          //   action="/api/admin/login"
+        >
           <div className="space-y-2">
             <label htmlFor="email">Email</label>
             <input
               id="email"
               name="email"
-              type="email"
-              placeholder="admin@example.com"
+              type="text"
+              placeholder="admin-user"
+              onChange={(e) => setUsername(e.target.value)}
               required
             />
           </div>
           <div className="space-y-2">
             <label htmlFor="password">Password</label>
-            <input id="password" name="password" type="password" required />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <button type="submit" className="w-full">
             Sign In
