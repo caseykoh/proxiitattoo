9814eb745b68b493eea4d3b712cf00e0304ee2e2
Casey Koh <s.casey.koh@gmail.com>
2025-01-21 12:21:07 -0500

    info bio

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./Info.css";
 import goToTop from "../GoToTop";
+import profilepic from "../../public/assets/proxiiprofile.jpeg";
 
 const faq = [
   {
@@ -8,11 +9,11 @@ const faq = [
     questions: [
       {
         q: "How do I book?",
-        a: "Send me a dm on Instagram @proxii_dream. Please detail your design idea, placement and size. I will get back to you with my next availability. Feel free to send images of designs/works from my page.",
+        a: "Send me an email at proxiidream@gmail.com. Please detail your design idea, placement and size. I will get back to you with my next availability and feel free to send images of designs/works from my page. Note: the appointment is only confirmed and booked once a deposit is sent.",
       },
       {
         q: "Where are you located?",
-        a: "I work out of @000000000000_studio, a private studio near Bloor-Yonge.",
+        a: "I work out of @000000000000_studio, a private studio near Bloor-Yonge. The address will be sent once a deposit is sent.",
       },
     ],
   },
@@ -59,10 +60,42 @@ const Info = () => {
   return (
     <>
       <main className="info-page w-full md:w-[40vw] pt-32 px-4">
+        <section className="grid md:grid-cols-2">
+          <div>⋆｡𖦹°⭒˚｡⋆</div>
+          <div className="text-black/40 flex flex-col gap-3">
+            <span>
+              hi - im proxii, or proxii_dream on instagram. i've been tattooing
+              for three years and i like to create hazy soundscapes that flow
+              with the body
+            </span>
+            <img
+              src={profilepic}
+              alt="proxii"
+              className="border-solid border border-black p-[1px]"
+            />
+            <span>
+              i'm heavily inspired by music, atmospheric and bubbly, nature, the
+              idea of light and absence and poetry.
+            </span>
+            <span>
+              hope you enjoy your stay on this little site of mine that i custom
+              coded & designed.
+            </span>
+            <span>
+              {
+                "thanks for your continued support and interest in my art <3 you make it all worthwhile"
+              }
+            </span>
+          </div>
+        </section>
+        <div className="py-12 text-center text-black/20">
+          <p className="">ฅ^._.^ฅ</p>
+          <p className="text-2xl">FAQ</p>
+        </div>
         <section className="">
           {faq.map(({ title, questions }, index) => (
             <div key={index} className="mb-10">
-              <h2 className="text-xl mb-4">{title}</h2>
+              <h2 className="text-2xl mb-5 font-mono">{title}</h2>
               <div className="questions">
                 {questions.map(({ q, a }, qIndex) => (
                   <div key={qIndex} className="faq-item">
