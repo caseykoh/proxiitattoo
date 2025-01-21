1b0541dd4f559b1e398ea967e9f837ac711af0f0
Casey Koh <s.casey.koh@gmail.com>
2025-01-21 12:51:18 -0500

    further styling

--- src/components/StoryStripe.tsx
+++ src/components/StoryStripe.tsx
@@ -1,6 +1,6 @@
 export function StoryStripe() {
   return (
-    <div className="absolute top-5 story-text pointer-events-none mx-4 tracking-tighter text-lime-300 flex flex-row">
+    <div className="absolute top-5 md:left-32 story-text pointer-events-none mx-4 tracking-tighter text-lime-300 flex flex-row">
       <p>a song of the invisible, a proxii_dream...</p>
       <p> waves of essence rise to the surface of the body...</p>
       <p>things only you could hear...</p>
